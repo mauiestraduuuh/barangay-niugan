a79c6b65ab8b71b3aaa1b92b4b12dc37d5347852
mauiestraduuuh <maureen.estrada@tup.edu.ph>
2025-11-11 23:18:36 +0800

    db changes pls prisma generate

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,71 +1,73 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, ComplaintGroup } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
 async function main() {
-  console.log("Seeding Feedback Categories...");
+  console.log("Seeding Complaint Categories...");
 
-  const categories= [
-    // peace
-    { name_en: "Noisy karaoke late at night", name_tl: "Maingay na karaoke hanggang disoras ng gabi" },
-    { name_en: "Noisy gatherings of youth on the street", name_tl: "Maingay na tambayan ng mga kabataan sa kalsada" },
-    { name_en: "Loud vehicle or motorcycle exhaust", name_tl: "Maingay na tambutso ng sasakyan o motor" },
-    { name_en: "Neighbors gossiping or spreading rumors", name_tl: "Mga kapitbahay na mahilig sa tsismis o paninira" },
-    { name_en: "Neighbors fighting in the middle of the street", name_tl: "Nag-aaway na kapitbahay sa gitna ng kalsada" },
-    { name_en: "Youth not following curfew", name_tl: "Mga kabataang hindi sumusunod sa curfew" },
-    { name_en: "Drinking sessions on the street or in front of houses", name_tl: "Mga nag-iinuman sa kalsada o harap ng bahay" },
+  const complaints = [
+    // PEACE
+    { english: "Noisy karaoke late at night", tagalog: "Maingay na karaoke hanggang disoras ng gabi", group: ComplaintGroup.PEACE },
+    { english: "Noisy gatherings of youth on the street", tagalog: "Maingay na tambayan ng mga kabataan sa kalsada", group: ComplaintGroup.PEACE },
+    { english: "Loud vehicle or motorcycle exhaust", tagalog: "Maingay na tambutso ng sasakyan o motor", group: ComplaintGroup.PEACE },
+    { english: "Neighbors gossiping or spreading rumors", tagalog: "Mga kapitbahay na mahilig sa tsismis o paninira", group: ComplaintGroup.PEACE },
+    { english: "Neighbors fighting in the middle of the street", tagalog: "Nag-aaway na kapitbahay sa gitna ng kalsada", group: ComplaintGroup.PEACE },
+    { english: "Youth not following curfew", tagalog: "Mga kabataang hindi sumusunod sa curfew", group: ComplaintGroup.PEACE },
+    { english: "Drinking sessions on the street or in front of houses", tagalog: "Mga nag-iinuman sa kalsada o harap ng bahay", group: ComplaintGroup.PEACE },
 
-    // pets
-    { name_en: "Dogs chasing people", name_tl: "Nanghahabol na aso" },
-    { name_en: "Pets defecating anywhere", name_tl: "Mga alagang hayop na kung saan-saan pinapatae" },
-    { name_en: "Smelly piggery or poultry area", name_tl: "Mababahong babuyan o manukan" },
-    { name_en: "Animal abuse (hurt, starved, or slaughtered cruelly)", name_tl: "Pang-aabuso sa hayop (sinusugatan, ginugutom, o kinakatay)" },
-    { name_en: "Unleashed or stray pets roaming freely", name_tl: "Walang tali o kulungan ang mga alagang hayop na nagpapakalat-kalat sa daan" },
+    // PETS
+    { english: "Dogs chasing people", tagalog: "Nanghahabol na aso", group: ComplaintGroup.PETS },
+    { english: "Pets defecating anywhere", tagalog: "Mga alagang hayop na kung saan-saan pinapatae", group: ComplaintGroup.PETS },
+    { english: "Smelly piggery or poultry area", tagalog: "Mababahong babuyan o manukan", group: ComplaintGroup.PETS },
+    { english: "Animal abuse (hurt, starved, or slaughtered cruelly)", tagalog: "Pang-aabuso sa hayop (sinusugatan, ginugutom, o kinakatay)", group: ComplaintGroup.PETS },
+    { english: "Unleashed or stray pets roaming freely", tagalog: "Walang tali o kulungan ang mga alagang hayop na nagpapakalat-kalat sa daan", group: ComplaintGroup.PETS },
 
-    // environment
-    { name_en: "Improper garbage disposal", name_tl: "Pagtatapon ng basura sa hindi tamang lugar" },
-    { name_en: "Clogged or overflowing drainage", name_tl: "Umaapaw o baradong drainage/kanal" },
-    { name_en: "Irregular garbage collection", name_tl: "Hindi regular ang pangongolekta ng basura" },
-    { name_en: "Dirty streets not cleaned by sweepers", name_tl: "Maruruming kalsada o hindi nililinis ng street sweeper" },
-    { name_en: "Foul-smelling garbage pile in one area", name_tl: "Mababahong tambak ng basura sa isang lugar" },
-    { name_en: "Burning trash despite prohibition", name_tl: "Pagsusunog ng basura kahit bawal (pagsisiga)" },
+    // ENVIRONMENT
+    { english: "Improper garbage disposal", tagalog: "Pagtatapon ng basura sa hindi tamang lugar", group: ComplaintGroup.ENVIRONMENT },
+    { english: "Clogged or overflowing drainage", tagalog: "Umaapaw o baradong drainage/kanal", group: ComplaintGroup.ENVIRONMENT },
+    { english: "Irregular garbage collection", tagalog: "Hindi regular ang pangongolekta ng basura", group: ComplaintGroup.ENVIRONMENT },
+    { english: "Dirty streets not cleaned by sweepers", tagalog: "Maruruming kalsada o hindi nililinis ng street sweeper", group: ComplaintGroup.ENVIRONMENT },
+    { english: "Foul-smelling garbage pile in one area", tagalog: "Mababahong tambak ng basura sa isang lugar", group: ComplaintGroup.ENVIRONMENT },
+    { english: "Burning trash despite prohibition", tagalog: "Pagsusunog ng basura kahit bawal (pagsisiga)", group: ComplaintGroup.ENVIRONMENT },
 
-    // road
-    { name_en: "Double parking on narrow roads", name_tl: "Double parking sa makitid na daan" },
-    { name_en: "Vehicles blocking driveways", name_tl: "Mga sasakyang humaharang sa driveway" },
-    { name_en: "Illegal tricycle or motorcycle parking", name_tl: "Ilegal na paradahan ng tricycle o motorsiklo" },
-    { name_en: "Broken or missing traffic signs/speed bumps", name_tl: "Sira o kulang sa traffic signage at speed bump" },
-    { name_en: "Overspeeding vehicles endangering pedestrians", name_tl: "Mga matutuling magpatakbo ng sasakyan na delikado sa mga naglalakad" },
+    // ROAD
+    { english: "Double parking on narrow roads", tagalog: "Double parking sa makitid na daan", group: ComplaintGroup.ROAD },
+    { english: "Vehicles blocking driveways", tagalog: "Mga sasakyang humaharang sa driveway", group: ComplaintGroup.ROAD },
+    { english: "Illegal tricycle or motorcycle parking", tagalog: "Ilegal na paradahan ng tricycle o motorsiklo", group: ComplaintGroup.ROAD },
+    { english: "Broken or missing traffic signs/speed bumps", tagalog: "Sira o kulang sa traffic signage at speed bump", group: ComplaintGroup.ROAD },
+    { english: "Overspeeding vehicles endangering pedestrians", tagalog: "Mga matutuling magpatakbo ng sasakyan na delikado sa mga naglalakad", group: ComplaintGroup.ROAD },
 
-    // ingfrastructure
-    { name_en: "Broken street light post", name_tl: "Sirang poste ng ilaw sa kalsada" },
-    { name_en: "Defective barangay CCTV camera", name_tl: "Sirang CCTV camera ng barangay" },
-    { name_en: "Hanging or exposed live wires", name_tl: "Nakalaylay o nakasabit na live wires" },
-    { name_en: "Damaged or pothole-filled roads", name_tl: "Butas-butas o sirang kalsada" },
-    { name_en: "Lack of street lights in dark areas", name_tl: "Kulang sa street lights sa madidilim na lugar" },
+    // INFRASTRUCTURE
+    { english: "Broken street light post", tagalog: "Sirang poste ng ilaw sa kalsada", group: ComplaintGroup.INFRASTRUCTURE },
+    { english: "Defective barangay CCTV camera", tagalog: "Sirang CCTV camera ng barangay", group: ComplaintGroup.INFRASTRUCTURE },
+    { english: "Hanging or exposed live wires", tagalog: "Nakalaylay o nakasabit na live wires", group: ComplaintGroup.INFRASTRUCTURE },
+    { english: "Damaged or pothole-filled roads", tagalog: "Butas-butas o sirang kalsada", group: ComplaintGroup.INFRASTRUCTURE },
+    { english: "Lack of street lights in dark areas", tagalog: "Kulang sa street lights sa madidilim na lugar", group: ComplaintGroup.INFRASTRUCTURE },
 
-    // safety
-    { name_en: "Child abuse or neglect", name_tl: "Pang-aabuso o pagpapabaya sa bata" },
-    { name_en: "Domestic abuse or family violence", name_tl: "Domestic abuse o karahasan sa pamilya" },
-    { name_en: "Obstruction on roads (e.g. plants, trash, debris)", name_tl: "May nakaharang sa daan (hal. halaman, basura, tambak na lupa, atbp.)" },
+    // SAFETY
+    { english: "Child abuse or neglect", tagalog: "Pang-aabuso o pagpapabaya sa bata", group: ComplaintGroup.SAFETY },
+    { english: "Domestic abuse or family violence", tagalog: "Domestic abuse o karahasan sa pamilya", group: ComplaintGroup.SAFETY },
+    { english: "Obstruction on roads (e.g. plants, trash, debris)", tagalog: "May nakaharang sa daan (hal. halaman, basura, tambak na lupa, atbp.)", group: ComplaintGroup.SAFETY },
   ];
 
-  for (const category of categories) {
+  for (const complaint of complaints) {
     await prisma.complaintCategory.upsert({
-      where: { english_name: category.name_en },
+      where: { english_name: complaint.english },
       update: {},
       create: {
-        tagalog_name: category.name_tl,
-        english_name: category.name_en,
+        english_name: complaint.english,
+        tagalog_name: complaint.tagalog,
+        group: complaint.group,
         description: null,
       },
     });
   }
 
-  console.log("Complaint categories seeded successfully!");
+  console.log("31 Complaint categories seeded successfully!");
 }
 
 main()
-  .catch((e) => {
+  .catch(e => {
     console.error(e);
     process.exit(1);
   })
